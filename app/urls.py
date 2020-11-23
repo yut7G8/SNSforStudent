79384620297ae41127fbd27d8987426a2e91884b
yut7G8 <billion7g8.316@keio.jp>
2020-11-23 21:02:27 +0900

    制限付きいいね機能

--- app/urls.py
+++ app/urls.py
@@ -28,15 +28,15 @@ urlpatterns = [
     path('view_societies/<int:pk>',view_societies,name='view_societies'),
     path('detail_society/<int:pk>/<int:id>/',detail_society,name='detail_society'),
 
-    #path('detail/<int:pk>', detailfunc, name='detail'),
+    # いいね機能用のURL++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     path('detail/<int:post_id>/', goodfunc, name='good'),
+
+    path('like/', views.like, name='like'),
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     path('student_profile/<int:pk>', student_profile, name='student_profile'),
     path('student_profile_update/<int:pk>',views.StudentProfileUpdate.as_view(),name='student_profile_update'),
-    #path('<slug:username>', views.StudentProfileDetailView.as_view(), name='profile'),
-    #path('profile/<email>', views.StudentProfileDetailView.as_view(), name='profile'),
-    #path('<slug:username>/edit', views.StudentProfileUpdateView.as_view(), name='edit'),
-    #path('student_profile/<int:pk>', views.StudentProfile.as_view(), name='student_profile'),
 
     path('society_profile/<int:pk>/', views.SocietyProfile.as_view(), name='society_profile'),
     path('society_profile_update/<int:pk>/', views.SocietyProfileUpdate.as_view(), name='society_profile_update'),
@@ -44,10 +44,7 @@ urlpatterns = [
     path('follow/<email>', views.follow_view, name='follow'),
     path('follow2/<email>', views.follow_from_detail, name='follow_from_detail'),
     path('unfollow/<email>', views.unfollow_view, name='unfollow'),
-    #path('<slug:username>/follow', views.follow_view, name='follow'),
-    #path('<slug:username>/unfollow', views.unfollow_view, name='unfollow'),
 
-    #path('detail/', detailfunc, name='detailfunc'), # views.pyのdetailfuncを参照
     path('detail/<int:post_id>/', views.everypost, name='everypost'), # views.pyのeverypost関数を参照
     # path('detail/<int:post_id>/', views.everypostforStuednt, name='everypostforStudent'), # 学生側の閲覧用everypage
     path('add/<int:pk>', views.add, name='add'), # 投稿フォーム用のpath(仮)設定
