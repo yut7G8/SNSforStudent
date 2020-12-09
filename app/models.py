21d3c1962fac55ab4ccb288079c1e2e09073932e
yut7G8 <billion7g8.316@keio.jp>
2020-12-09 21:16:06 +0900

    画像投稿は任意

--- app/models.py
+++ app/models.py
@@ -143,7 +143,7 @@ class BoardModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=100)
-    images = models.ImageField(upload_to='')
+    images = models.ImageField(upload_to='', blank=True)
     good = models.IntegerField(default=0)
     read = models.IntegerField(default=0)
     readtext = models.CharField(max_length=200)
